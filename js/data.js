00b4b02ff17ce40e4ef57f2c0b5f7125e2ab0cc4
Julián Amé <julianmmame@gmail.com>
2022-11-17 21:07:26 -0300

    creando la estructura de datos para las fichas¿

--- js/data.js
+++ js/data.js
@@ -9,6 +9,172 @@ const HEXWIDTH = 104;
 
 const CELLARRAY = [];
 
+const PIECEARRAY = [];
+const COLORARRAY = [];
+
+COLORARRAY.push(new Color(0, 'orange', '#ff5624'));
+COLORARRAY.push(new Color(COLORARRAY.length, 'magenta', '#872996'));
+COLORARRAY.push(new Color(COLORARRAY.length, 'green', '#44b94a'));
+COLORARRAY.push(new Color(COLORARRAY.length, 'gray', '#c1cac8'));
+COLORARRAY.push(new Color(COLORARRAY.length, 'yellow', '#ffe951'));
+COLORARRAY.push(new Color(COLORARRAY.length, 'blue', '#162061'));
+
+console.log(COLORARRAY);
+
+class Color {
+    constructor(id, name, color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+    get id() {
+        return this.id;
+    }
+    get name() {
+        return this.name;
+    }
+    get color() {
+        return this.color;
+    }
+}
+
+class Piece {
+    constructor(pieceId, player) {
+        this.pieceId = pieceId;
+        this.player = player;
+        //Define Colours depending on piece
+        switch (pieceId) {
+            case 1:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                break;
+            case 2:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                break;
+            case 3:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                break;
+            case 4:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                break;
+            case 5:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                break;
+            case 6:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 7:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 8:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 9:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 10:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 11:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+            case 12:
+                this.color_top_left = COLORARRAY[0];
+                this.color_top_right = COLORARRAY[0];
+                this.color_middle_left = COLORARRAY[0];
+                this.color_middle_right = COLORARRAY[0];
+                this.color_bottom_right = COLORARRAY[0];
+                this.color_bottom_left = COLORARRAY[0];
+                break;
+        }
+        this.cellId = 0;
+    }
+    get getCellId(){
+        return this.cellId;
+    }
+    get PieceId(){
+        return this.pieceId;
+    }
+    get Player(){
+        return this.player;
+    }
+    get color_top_left(){
+        return this.color_top_left;
+    }
+    get color_top_right(){
+        return this.color_top_left;
+    }
+    get color_middle_right(){
+        return this.color_top_left;
+    }
+    get color_top_left(){
+        return this.color_top_left;
+    }
+    get color_top_left(){
+        return this.color_top_left;
+    }
+    get color_top_left(){
+        return this.color_top_left;
+    }
+
+    set setCellId(newCell){
+        this.cellId = newCell;
+    }
+}
 
 class Cell {
     constructor() {
